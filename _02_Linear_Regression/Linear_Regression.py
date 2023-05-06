b6dff6b20d336871267fac985abfe0a5249f7ac6
= <=>
2023-05-06 11:03:32 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -12,7 +12,25 @@ def ridge(data):
     pass
     
 def lasso(data):
-    pass
+     #梯度下降法实现Lasso回归
+    X, y = read_data()
+    # 超参数
+    alpha = 0.01
+    epoch = 10000
+    lr = 0.001
+    # 初始化w
+    w = np.zeros(X.shape[1])
+    # 梯度下降
+    for i in range(epoch):
+        # 梯度
+        grad = np.matmul(X.T, np.matmul(X, w) - y) / X.shape[0] + alpha * np.sign(w)
+        # 防止梯度爆炸
+        grad_norm = np.linalg.norm(grad)
+        if grad_norm > 1:
+            grad /= grad_norm
+        # 更新梯度
+        w -= lr * grad
+    return np.matmul(w, data)
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
