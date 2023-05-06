a96655f4915611464e0ecb4d1c079fab00b30ef4
= <=>
2023-05-06 11:29:45 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,18 +8,35 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
+#最小二乘实现岭回归
 def ridge(data):
-    pass
-    
-def lasso(data):
-     #梯度下降法实现Lasso回归
+
     X, y = read_data()
+
+    # 样本数量和特征维度
+    N, D = X.shape
+
+    # 超参数
+    alpha = 0.05
+
+    A = np.eye(D) * alpha
+    w_ridge = np.linalg.inv(X.T * X + alpha * A) @ X.T * y
+
+    return w_ridge * data
+
+#梯度下降法实现Lasso回归    
+def lasso(data):  
+
+    X, y = read_data()
+
     # 超参数
     alpha = 0.01
     epoch = 10000
     lr = 0.001
+
     # 初始化w
     w = np.zeros(X.shape[1])
+
     # 梯度下降
     for i in range(epoch):
         # 梯度
@@ -30,6 +47,7 @@ def lasso(data):
             grad /= grad_norm
         # 更新梯度
         w -= lr * grad
+
     return np.matmul(w, data)
 
 def read_data(path='./data/exp02/'):
